584f743f9c79e82f6107bf34d07576e3a5cb9c07
Thomas Kottke <t.kottke90@gmail.com>
2024-11-10 15:26:27 -0600

    refactor: add create stream to controller

--- backend/src/controllers/stream.controller.ts
+++ backend/src/controllers/stream.controller.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import {
-  Body,
   Controller,
   Response,
   Request,
   Next,
-  Post
+  Post,
+  Query,
+  Get
 } from '@decorators/express';
 import passport from 'passport';
 import { StreamRoute } from '../routes';
@@ -13,6 +14,7 @@ import { Inject } from '@decorators/di';
 import { StreamDao, StreamDaoIdentifier } from '../dao/stream.dao';
 import { StreamFindDTO, StreamFindSchema } from '../dto/stream.dto';
 import { AuthenticatedUser } from '../interfaces/auth.interfaces';
+import { randomUUID } from 'crypto';
 
 @Controller(StreamRoute.path, [
   passport.authenticate('cookie', { session: false })
@@ -20,17 +22,36 @@ import { AuthenticatedUser } from '../interfaces/auth.interfaces';
 export default class ServerStatusController {
   constructor(@Inject(StreamDaoIdentifier) readonly streamDao: StreamDao) {}
 
-  @Post('/')
-  async getRoot(
+  @Get('/')
+  async getStreams(
     @Response() res: express.Response,
-    @Body() body: StreamFindDTO,
+    @Query() filter: StreamFindDTO,
     @Request('user') user: AuthenticatedUser,
     @Next() next: express.NextFunction
   ) {
     try {
-      const query = StreamFindSchema.parse({ ...body, ownerId: user.id });
+      const query = StreamFindSchema.parse({ ...filter, ownerId: user.id });
 
       const result = await this.streamDao.get(query);
+
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  @Post('/')
+  async createStream(
+    @Response() res: express.Response,
+    @Request('user') user: AuthenticatedUser,
+    @Next() next: express.NextFunction
+  ) {
+    try {
+      const result = await this.streamDao.create({
+        ownerId: user.id,
+        key: randomUUID(),
+        url: 'http://stream-helper.tdkottke.com'
+      });
 
       res.json(result);
     } catch (error) {
