8210b12d98f3d0377664cc0fbeef4b8a5a3e4d49
Thomas Kottke <t.kottke90@gmail.com>
2024-11-13 07:41:57 -0600

    refactor: add logs to entrypoint.sh

--- backend/bin/entrypoint.sh
+++ backend/bin/entrypoint.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+echo "##################################################"
+echo "Stream Helper API"
+echo 
+echo "  Environment:"
+echo "    DB Type:   $DATABASE_SCHEMA"
+exho "    DB Target: $DATABASE_URL"
+echo 
+echo "##################################################"
+echo
+
+# Execute the migration to make sure the database stays up to date
+echo "> Executing Migrations"
 node ./node_modules/prisma/build/index.js migrate deploy
 
+# Start the server
+echo " > Starting App"
 exec node dist/index.js
