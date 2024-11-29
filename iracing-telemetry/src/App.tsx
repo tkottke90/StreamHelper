490fca642aa7fc79617aaf71f449681f5de194fd
Thomas Kottke <t.kottke90@gmail.com>
2024-11-29 12:12:50 -0600

    refactor: move styles into common module

--- iracing-telemetry/src/App.tsx
+++ iracing-telemetry/src/App.tsx
@@ -1,6 +1,5 @@
-import "./App.scss";
 import { batch, useSignal, useSignalEffect } from "@preact/signals";
-import { useConfigService } from "./services/config.service";
+import { ConfigContext, useConfigContext } from "./services/config.service";
 import { useDbPath } from "./services/db.service";
 
 function classMap(baseClass: string, options: Record<string, boolean>) {
@@ -8,60 +7,56 @@ function classMap(baseClass: string, options: Record<string, boolean>) {
 }
 
 function App() {
-  const { loadConfigs } = useConfigService();
   const dbPath = useDbPath();
 
   const loading = useSignal(true);
-  const config = useSignal<Record<string, any>>({});
-  const error = useSignal<string>('');
-
-  useSignalEffect(() => {
-    new Promise(async (resolve) => {
-      let result: any;
-
-      try {
-        result = await loadConfigs({})
-        console.dir(result)
-      } catch (err: any) {
-        error.value = err.message;
-      }
-
-      batch(() => {
-        loading.value = false;
-        config.value = result;
-      })
-      loading.value = false;
-      resolve(null);
-    })
-  });
 
   return (
-    <div className="w-full h-full m-0 grid grid-cols-[200px_1fr] grid-rows-[56px_1fr] relative">
-      <div className={classMap("absolute inset-0 bg-slate-950 opacity-0 origin-center scale-0", { 'scale-100': loading.value })}>
-        {/* scrim */}
+    <ConfigContext>
+      <div className="w-full h-full m-0 grid grid-cols-[200px_1fr] grid-rows-[56px_1fr] relative">
+        <div className={classMap("absolute inset-0 bg-slate-950 opacity-0 origin-center scale-0", { 'scale-100': loading.value })}>
+          {/* scrim */}
 
+        </div>
+        <header className="py-2 px-4 col-span-2">
+          <h2 className="m-0">Stream Helper - iRacing Telemetry Listener</h2>
+        </header>
+        <main className="p-4 col-span-2">
+          <section>
+            <p>Path: <span>{ dbPath.loading.value ? 'Loading Path' : dbPath.value.value }</span></p>
+          </section>
+          <br />
+          <br />
+          <ConfigDisplay />
+        </main>
       </div>
-      <header className="py-2 px-4 col-span-2">
-        <h2 className="m-0">Stream Helper - iRacing Telemetry Listener</h2>
-      </header>
-      <aside></aside>
-      <main className="p-4">
-        <section>
-          <p>Path: <span>{ dbPath.loading.value ? 'Loading Path' : dbPath.value.value }</span></p>
-        </section>
-        <br />
-        <br />
-        <section>
-          <div>
-            <span>Config: </span>
-            <pre>
-              { error.value ? `ERROR: ${error.value}` : JSON.stringify(config.value, null, 2) }
-            </pre>
-          </div>
-        </section>
-      </main>
-    </div>
+    </ConfigContext>
   );
+}
+
+function ConfigDisplay() {
+  const { configs } = useConfigContext();
+
+  return (
+    <section>
+      <div>
+        <div className="flex justify-between">
+          <h2>Configuration</h2>
+          <div>
+            <button className="btn-accent--raised">Raw Editor</button>
+          </div>
+        </div>
+        <div>
+          <p>iRacing Directory:</p>
+          <p>{configs.value.iracingUrl}</p>
+        </div>
+
+        {/* <pre>
+          { JSON.stringify(configs.value, null, 2) }
+        </pre> */}
+      </div>
+    </section>
+  )
 }
 
 export default App;
