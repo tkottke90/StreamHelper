6bb93ae7a4dcee046c81be92fafd4258be257ba1
Thomas Kottke <t.kottke90@gmail.com>
2025-12-19 09:26:43 -0600

    feat: add error details to websocket auth error

--- backend/src/websockets/errors.ts
+++ backend/src/websockets/errors.ts
@@ -5,6 +5,6 @@ export class WebsocketAuthError extends BaseError {
   name = 'WebsocketAuthError'
 
   constructor(clientId: string, ip: string, event: string) {
-    super(`Unauthenticated WebSocket client attempted to access protected event(${event}): ${clientId} - ${ip}`);
+    super(`Unauthenticated WebSocket client attempted to access protected a event`, { clientId, ip, event });
   }
 }
